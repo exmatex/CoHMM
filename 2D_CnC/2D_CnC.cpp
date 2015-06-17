f1adaed527fb5d31083520d79d5c9a860ffc4f3a
Robert Pavel <rspavel@lanl.gov>
2015-06-17 16:36:38 -0600

    CnC Context Set Up

--- 2D_CnC/2D_CnC.cpp
+++ 2D_CnC/2D_CnC.cpp
@@ -1,5 +1,120 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "CoHMM_DaD.hpp"
+
+#include "2D_CnC.hpp"
+
+DaDContext::DaDContext()
+	:
+	CnC::context<DaDContext>(),
+	fluxTags(*this, "fluxTags"),
+	fluxTask(*this, "fluxTasks"),
+	globalItem(*this, "globalItem")
+{
+	//Indicate which tags correspond to which steps
+	fluxTags.prescribes(fluxTask, *this);
+
+	//Indicate which step consumes which item (ha ha)
+	fluxTask.consumes(globalItem);
+}
+
+int Flux_Task::execute(const Flux_Tag &tag, DaDContext &c) const
+{
+	//Get The Item
+	Flux_Item runConfig;
+	c.globalItem.get(0, runConfig);
+	//Get params
+	unsigned int step = std::get<0>(tag);
+	unsigned int phase = std::get<1>(tag);
+	unsigned int task = std::get<2>(tag);
+	//Call
+	cloudFlux(runConfig.doKriging, runConfig.doCoMD, step, phase, task, runConfig.redis_host);
+	//Return
+	return CnC::CNC_Success;
+}
+
 int main(int argc, char ** argv)
 {
+	//dimX dimY nSteps redis_server 
+	if( argc != 5)
+	{
+		std::cerr <<  "./2D_DaDTest <dim_x> <dim_y> <nsteps> <redis_server>" << std::endl;
+		return 1;
+	}
+	//Set up parameters
+	bool doKriging = true;
+	bool doCoMD = false;
+	int dims[2] = {atoi(argv[1]), atoi(argv[2])};
+	double dt[2] = {0.1, 0.1};
+	double delta[2] = {1.0, 1.0};
+	double gamma[3];
+	gamma[0] = 0; //mom_gamma
+	gamma[1] = gamma[0]; //strain_gamma
+	gamma[2] = 0.1 * gamma[1];//en_gamma
+
+	unsigned int numSteps = atoi(argv[3]);
+
+	//Initialize
+	std::cout << "Initializing " << dims[0] << " by " << dims[1] << " grid" << std::endl;
+	initEverything(doKriging, doCoMD, dims, dt, delta, gamma);
+	std::cout << "Initialized" << std::endl;
+	//Loop
+	std::cout << "Running for " << numSteps << " iterations" << std::endl;
+	for(unsigned int t = 0; t < numSteps; t++)
+	{
+		int nTasks;
+		std::cout << t << ": Vising to Verifying" << std::endl;
+		outputVTK(doKriging, doCoMD, dims, dt, delta, gamma, t, argv[4]);
+		//Do a short circuit test
+		if(tryShortCircuit(dims, t, argv[4]))
+		{
+			//Short circuit succeeded
+			std::cout << t << ": Short Circuit Successful, on to the next step!" << std::endl;
+		}
+		else
+		{
+			std::cout << t << ": First Flux" << std::endl;
+			nTasks = prepFirstFlux(doKriging, doCoMD, dims, dt, delta, gamma, t, argv[4]);
+			std::cout << t << ": Doing " << nTasks << " fluxes" << std::endl;
+			#pragma omp parallel for
+			for(unsigned int i = 0; i < nTasks; i++)
+			{
+				cloudFlux(doKriging, doCoMD, t, 0, i, argv[4]);
+			}
+			std::cout << t << ": Second Flux" << std::endl;
+			nTasks = prepSecondFlux(doKriging, doCoMD, dims, dt, delta, gamma, t, argv[4]);
+			std::cout << t << ": Doing " << nTasks << " fluxes" << std::endl;
+			#pragma omp parallel for
+			for(unsigned int i = 0; i < nTasks; i++)
+			{
+				cloudFlux(doKriging, doCoMD, t, 1, i, argv[4]);
+			}
+			std::cout << t << ": Third Flux" << std::endl;
+			nTasks = prepThirdFlux(doKriging, doCoMD, dims, dt, delta, gamma, t, argv[4]);
+			std::cout << t << ": Doing " << nTasks << " fluxes" << std::endl;
+			#pragma omp parallel for
+			for(unsigned int i = 0; i < nTasks; i++)
+			{
+				cloudFlux(doKriging, doCoMD, t, 2, i, argv[4]);
+			}
+			std::cout << t << ": Last Flux" << std::endl;
+			nTasks = prepLastFlux(doKriging, doCoMD, dims, dt, delta, gamma, t, argv[4]);
+			std::cout << t << ": Doing " << nTasks << " fluxes" << std::endl;
+			#pragma omp parallel for
+			for(unsigned int i = 0; i < nTasks; i++)
+			{
+				cloudFlux(doKriging, doCoMD, t, 3, i, argv[4]);
+			}
+			std::cout << t << ": Finish Step, no Fluxes" << std::endl;
+			finishStep(doKriging, doCoMD, dims, dt, delta, gamma, t, argv[4]);
+		}
+	}
+	//Final vis
+	std::cout << numSteps << ": Vising to Verifying" << std::endl;
+	outputVTK(doKriging, doCoMD, dims, dt, delta, gamma, numSteps, argv[4]);
+	std::cout << "Ran for " << numSteps << " iterations" << std::endl;
+
 	return 0;
 }
 
---
+++ 2D_CnC/2D_CnC.hpp
@@ -0,0 +1,53 @@
+#ifndef TWOD_CNC_HPP
+#define TWOD_CNC_HPP
+
+#include <tuple>
+
+#include <cnc/cnc.h>
+
+#include "CoHMM_DaD.hpp"
+
+const unsigned int MAX_HOST_LENGTH = 64;
+
+//Forward delcaration
+struct DaDContext;
+
+//Typedef for tag (step, phase, task)
+typedef std::tuple<int, int, int> Flux_Tag; 
+
+//The one singleton item
+struct Flux_Item
+{
+	char redis_host[MAX_HOST_LENGTH];
+	bool doKriging;
+	bool doCoMD;
+};
+CNC_BITWISE_SERIALIZABLE(Flux_Item);
+
+//Functors to act as steps
+struct Flux_Task
+{
+	int execute(const Flux_Tag &tag, DaDContext &c) const;
+};
+
+//Tuners for garbage collection
+///TODO: Do this
+
+//Context
+struct DaDContext : public CnC::context<DaDContext>
+{
+	//Flux Tags
+	CnC::tag_collection<Flux_Tag> fluxTags;
+
+	//Step
+	CnC::step_collection<Flux_Task> fluxTask;
+
+	//The loneliest item
+	CnC::item_collection<int, Flux_Item> globalItem;
+
+	//No-arg constructor
+	DaDContext();
+};
+
+#endif
+
