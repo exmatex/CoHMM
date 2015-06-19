96bf6815966d959996fe309788b3388f57b1127f
Robert Pavel <rspavel@lanl.gov>
2015-06-19 15:56:49 -0600

    Committing and Pushing failed attempt at Libcircle
    Another attempt to follow, maybe

--- 2D_Libcircle/2D_Libcircle.cpp
+++ 2D_Libcircle/2D_Libcircle.cpp
@@ -1,7 +1,121 @@
 #include <iostream>
 #include <cstdlib>
+#include <cstring>
+
+#include <mpi.h>
+#include <libcircle.h>
+
 
 #include "CoHMM_DaD.hpp"
+
+//Rely on globals for nTasks, step, and phase. At least for spawning purposes
+unsigned int step;
+unsigned int phase;
+unsigned int nTasks;
+bool doKriging;
+bool doCoMD;
+char redis_host[48];
+
+//Methods to convert tasks to libcircle tasks (strings)
+void buildTaskString(char * buffer, bool doKriging, bool doCoMD, unsigned int step, unsigned int phase, unsigned int tid, const char * redis_host)
+{
+	//Space delimited because that can't be in a hostname for obvious reasons
+	sprintf(buffer, "%d %d %d %d %d  %s", doKriging, doCoMD, step, phase, tid, redis_host);
+}
+
+void unBuildTaskString(char * taskString, bool & doKriging, bool & doCoMD, unsigned int & step, unsigned int & phase, unsigned int & tid, char * redis_host)
+{
+	//Tokenize and convert the string
+	//First is kriging
+	char * token = std::strtok(taskString, " ");
+	if(atoi(token) == 1)
+	{
+		doKriging = true;
+	}
+	else
+	{
+		doKriging = false;
+	}
+	//Next is comd
+	token = std::strtok(nullptr, " ");
+	if(atoi(token) == 1)
+	{
+		doCoMD = true;
+	}
+	else
+	{
+		doCoMD = false;
+	}
+	//Now step
+	token = std::strtok(nullptr, " ");
+	step = atoi(token);
+	//Phase
+	token = std::strtok(nullptr, " ");
+	phase = atoi(token);
+	//tid
+	token = std::strtok(nullptr, " ");
+	phase = atoi(token);
+	//Host
+	token = std::strtok(nullptr, " ");
+	strcpy(redis_host, token);
+}
+
+void processTasks(CIRCLE_handle *handle)
+{
+	char taskString[96];
+	//Dominic did reference thing and it worked
+	handle->dequeue(&taskString[0]);
+	bool krig;
+	bool comd;
+	unsigned int t;
+	unsigned int p;
+	unsigned int i;
+	char redis[48];
+	unBuildTaskString(taskString, krig, comd, t, p, i, redis);
+	std::cout << "Test" << std::endl;
+	cloudFlux(krig, comd, t, p, i, redis);
+	//Is libcircle okay with not having a return? Becuase we don't
+}
+
+void buildTasks(CIRCLE_handle *handle)
+{
+	//If this doesn't happen on rank 0, it fails horribly
+	///TODO: is it more efficient to stack allocate before or during loop?
+	char taskString[96];
+	std::cout << "nTasks: " << nTasks << std::endl;
+	for(unsigned int i = 0; i < nTasks; i++)
+	{
+		buildTaskString(taskString, doKriging, doCoMD, step, phase, i, redis_host);
+		handle->enqueue(taskString);
+	}
+}
+
+void doLibcircleTasks()
+{
+	///TODO: Is there a way to not pay the cost of init and finalize on every single flux?
+	//Init libcircle(?): Don't pass args as we do that stuff on our own
+	int rank = CIRCLE_init(0, nullptr, CIRCLE_DEFAULT_FLAGS);
+	//Run task creation method
+	std::cout << "Trying to build?" << std::endl;
+	CIRCLE_cb_create(&buildTasks);
+	/*
+	CIRCLE_handle * handle = CIRCLE_get_handle();
+	char taskString[96];
+	for(unsigned int i = 0; i < nTasks; i++)
+	{
+		buildTaskString(taskString, doKriging, doCoMD, step, phase, i, redis_host);
+		//buildTaskString(taskString, false , true , 4, 5, 2, "localhost");
+		handle->enqueue(taskString);
+	}
+	*/
+	//Say what to do with tasks
+	CIRCLE_cb_process(&processTasks);
+	//Run tasks
+	//CIRCLE_begin();
+	//Finalize libcircle (?)
+	CIRCLE_finalize();
+
+}
 
 int main(int argc, char ** argv)
 {
@@ -13,8 +127,8 @@ int main(int argc, char ** argv)
 		return 1;
 	}
 	//Set up parameters
-	bool doKriging = true;
-	bool doCoMD = false;
+	doKriging = true;
+	doCoMD = false;
 	int dims[2] = {atoi(argv[1]), atoi(argv[2])};
 	double dt[2] = {0.1, 0.1};
 	double delta[2] = {1.0, 1.0};
@@ -22,6 +136,7 @@ int main(int argc, char ** argv)
 	gamma[0] = 0; //mom_gamma
 	gamma[1] = gamma[0]; //strain_gamma
 	gamma[2] = 0.1 * gamma[1];//en_gamma
+	strcpy(redis_host, argv[4]);
 
 	unsigned int numSteps = atoi(argv[3]);
 
@@ -31,60 +146,46 @@ int main(int argc, char ** argv)
 	std::cout << "Initialized" << std::endl;
 	//Loop
 	std::cout << "Running for " << numSteps << " iterations" << std::endl;
-	for(unsigned int t = 0; t < numSteps; t++)
+	for(step = 0; step < numSteps; step++)
 	{
-		int nTasks;
-		std::cout << t << ": Vising to Verifying" << std::endl;
-		outputVTK(doKriging, doCoMD, dims, dt, delta, gamma, t, argv[4]);
+		std::cout << step << ": Vising to Verifying" << std::endl;
+		outputVTK(doKriging, doCoMD, dims, dt, delta, gamma, step, argv[4]);
 		//Do a short circuit test
-		if(tryShortCircuit(dims, t, argv[4]))
+		if(tryShortCircuit(dims, step, argv[4]))
 		{
 			//Short circuit succeeded
-			std::cout << t << ": Short Circuit Successful, on to the next step!" << std::endl;
+			std::cout << step << ": Short Circuit Successful, on to the next step!" << std::endl;
 		}
 		else
 		{
-			std::cout << t << ": First Flux" << std::endl;
-			nTasks = prepFirstFlux(doKriging, doCoMD, dims, dt, delta, gamma, t, argv[4]);
-			std::cout << t << ": Doing " << nTasks << " fluxes" << std::endl;
-			#pragma omp parallel for
-			for(unsigned int i = 0; i < nTasks; i++)
-			{
-				cloudFlux(doKriging, doCoMD, t, 0, i, argv[4]);
-			}
-			std::cout << t << ": Second Flux" << std::endl;
-			nTasks = prepSecondFlux(doKriging, doCoMD, dims, dt, delta, gamma, t, argv[4]);
-			std::cout << t << ": Doing " << nTasks << " fluxes" << std::endl;
-			#pragma omp parallel for
-			for(unsigned int i = 0; i < nTasks; i++)
-			{
-				cloudFlux(doKriging, doCoMD, t, 1, i, argv[4]);
-			}
-			std::cout << t << ": Third Flux" << std::endl;
-			nTasks = prepThirdFlux(doKriging, doCoMD, dims, dt, delta, gamma, t, argv[4]);
-			std::cout << t << ": Doing " << nTasks << " fluxes" << std::endl;
-			#pragma omp parallel for
-			for(unsigned int i = 0; i < nTasks; i++)
-			{
-				cloudFlux(doKriging, doCoMD, t, 2, i, argv[4]);
-			}
-			std::cout << t << ": Last Flux" << std::endl;
-			nTasks = prepLastFlux(doKriging, doCoMD, dims, dt, delta, gamma, t, argv[4]);
-			std::cout << t << ": Doing " << nTasks << " fluxes" << std::endl;
-			#pragma omp parallel for
-			for(unsigned int i = 0; i < nTasks; i++)
-			{
-				cloudFlux(doKriging, doCoMD, t, 3, i, argv[4]);
-			}
-			std::cout << t << ": Finish Step, no Fluxes" << std::endl;
-			finishStep(doKriging, doCoMD, dims, dt, delta, gamma, t, argv[4]);
+			std::cout << step << ": First Flux" << std::endl;
+			nTasks = prepFirstFlux(doKriging, doCoMD, dims, dt, delta, gamma, step, argv[4]);
+			std::cout << step << ": Doing " << nTasks << " fluxes" << std::endl;
+			phase = 0;
+			doLibcircleTasks();
+			std::cout << step << ": Second Flux" << std::endl;
+			nTasks = prepSecondFlux(doKriging, doCoMD, dims, dt, delta, gamma, step, argv[4]);
+			std::cout << step << ": Doing " << nTasks << " fluxes" << std::endl;
+			phase = 1;
+			doLibcircleTasks();
+			std::cout << step << ": Third Flux" << std::endl;
+			nTasks = prepThirdFlux(doKriging, doCoMD, dims, dt, delta, gamma, step, argv[4]);
+			std::cout << step << ": Doing " << nTasks << " fluxes" << std::endl;
+			phase = 2;
+			doLibcircleTasks();
+			std::cout << step << ": Last Flux" << std::endl;
+			nTasks = prepLastFlux(doKriging, doCoMD, dims, dt, delta, gamma, step, argv[4]);
+			std::cout << step << ": Doing " << nTasks << " fluxes" << std::endl;
+			phase = 3;
+			doLibcircleTasks();
+			std::cout << step << ": Finish Step, no Fluxes" << std::endl;
+			finishStep(doKriging, doCoMD, dims, dt, delta, gamma, step, argv[4]);
 		}
 	}
 	//Final vis
 	std::cout << numSteps << ": Vising to Verifying" << std::endl;
 	outputVTK(doKriging, doCoMD, dims, dt, delta, gamma, numSteps, argv[4]);
 	std::cout << "Ran for " << numSteps << " iterations" << std::endl;
-
 	return 0;
 }
 
