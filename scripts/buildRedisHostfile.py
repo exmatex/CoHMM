5edf9f6b5141d4cc0121cf4880a15e8184feae09
Robert Pavel <rspavel@lanl.gov>
2015-06-29 11:02:12 -0600

    Servers are assigned to a satisfactor level
    Should probably actually do the number of servers that are requested

--- scripts/buildRedisHostfile.py
+++ scripts/buildRedisHostfile.py
@@ -4,7 +4,7 @@ def genNodeList():
     #Get environmental variable
     #nodeString = os.environ['SLURM_JOB_NODELIST']
     #For debug/devel
-    nodeString = "cn[3,46,52,61,111-113,116-118]"
+    nodeString = "cn[3-9,46,52,61,111-113,116-118]"
     #Remove leading 'cn'
     nodeString = nodeString.lstrip('cn')
     nodeList = []
@@ -29,26 +29,40 @@ def genNodeList():
                 nodeList.append(int(token))
     return nodeList;
 
+#Attempts to break nodeList into numServers chunks
+#Does not guarantee numServers chunks, but will attempt to get close
 def assignServers(nodeList, numServers):
     serverList = []
     #See if this is one of the easy case
     if (numServers == 1):
         #Just one server, so return the first
-        serverList.append(nodeList[0])
-    elif (numServers >= len(nodeList)):
+        for node in nodeList:
+            serverList.append( (node, nodeList[0]) )
+    elif ( numServers >= len(nodeList) ):
         #Everyone is a server
-        serverList = nodeList
+        for node in nodeList:
+            serverList.append( (node, node) )
     else:
         #We need to assign servers
-        #TODO
-        serverList = []
+        #TODO: Make a more even distribution
+        #TODO: Guarantee we have numServers chunks
+        numNodes = len(nodeList)
+        nodesPerServer = numNodes / numServers;
+        remNodes = numNodes % numServers;
+        if(remNodes > 0):
+            nodesPerServer = nodesPerServer + 1
+        chunkList = [nodeList[i:i + nodesPerServer] for i in xrange(0, len(nodeList), nodesPerServer)]
+        #chunks[i][0] is a server
+        for chunk in chunkList:
+            for node in chunk:
+                serverList.append( (node, chunk[0]) )
     return serverList
 
 
 def main():
     nodeList = genNodeList();
-    serverList = assignServers(nodeList, 4)
-    print nodeList
+    serverList = assignServers(nodeList, 7)
+    print serverList
 
 if __name__ == "__main__":
     main()
