0558be5b24821375c582836474b36e17e7eccfc0
Robert Pavel <rspavel@lanl.gov>
2015-07-21 09:54:12 -0600

    Mostly adhering to pep8

--- scripts/buildRedisHostfile.py
+++ scripts/buildRedisHostfile.py
@@ -1,25 +1,26 @@
 import os
 import sys
 
+
 def genNodeList():
-    #Get environmental variable
+    # Get environmental variable
     nodeString = os.environ['SLURM_JOB_NODELIST']
-    #For debug/devel
-    #nodeString = "cn[14,3-9,46,52,61,111-113,116-118]"
-    #Remove leading 'cn'
+    # For debug/devel
+    # nodeString = "cn[14,3-9,46,52,61,111-113,116-118]"
+    # Remove leading 'cn'
     nodeString = nodeString.lstrip('cn')
     nodeList = []
     if (nodeString.startswith('[') == False):
-        #Just the one
+        # Just the one
         nodeList.append(int(nodeString))
     else:
-        #Strip the brackets, we no longer need them
+        # Strip the brackets, we no longer need them
         nodeString = nodeString.strip('[]')
-        #Tokenize the string
+        # Tokenize the string
         tokenList = nodeString.rsplit(',')
-        #Iterate over tokens
+        # Iterate over tokens
         for token in tokenList:
-            #Two cases: Single or range
+            # Two cases: Single or range
             if '-' in token:
                 inclusiveRange = token.rsplit('-')
                 start = int(inclusiveRange[0])
@@ -30,83 +31,89 @@ def genNodeList():
                 nodeList.append(int(token))
     return nodeList
 
-#Attempts to break nodeList into numServers chunks
-#Does not guarantee numServers chunks, but will attempt to get close
+
+# Attempts to break nodeList into numServers chunks
+# Does not guarantee numServers chunks, but will attempt to get close
 def assignServers(nodeList, numServers):
     serverPairs = []
     serverList = []
-    #See if this is one of the easy case
-    #TODO: Do we need the cases anymore now that we have smart-ish chunking?
+    # See if this is one of the easy case
+    # TODO: Do we need the cases anymore now that we have smart-ish chunking?
     if (numServers == 1):
-        #Just one server, so return the first
+        # Just one server, so return the first
         for node in nodeList:
-            serverPairs.append( (node, nodeList[0]) )
+            serverPairs.append((node, nodeList[0]))
         serverList.append(nodeList[0])
-    elif ( numServers >= len(nodeList) ):
-        #Everyone is a server
+    elif (numServers >= len(nodeList)):
+        # Everyone is a server
         for node in nodeList:
-            serverPairs.append( (node, node) )
+            serverPairs.append((node, node))
         serverList = nodeList
     else:
-        #We need to assign servers
-        #TODO: Make a more even distribution
-        #TODO: Guarantee we have numServers chunks
+        # We need to assign servers
+        # TODO: Make a more even distribution
+        # TODO: Guarantee we have numServers chunks
         numNodes = len(nodeList)
         nodesPerServer = numNodes / numServers
         remNodes = numNodes % numServers
         if(remNodes > 0):
             nodesPerServer = nodesPerServer + 1
         chunkList = [nodeList[i:i + nodesPerServer] for i in xrange(0, len(nodeList), nodesPerServer)]
-        #chunks[i][0] is a server
+        # chunks[i][0] is a server
         for chunk in chunkList:
             serverList.append(chunk[0])
             for node in chunk:
-                serverPairs.append( (node, chunk[0]) )
+                serverPairs.append((node, chunk[0]))
     return (serverList, serverPairs)
 
+
 def writeLUTFile(serverPairs):
-    #Open file to write
+    # Open file to write
     lFile = open('lutFile', 'w')
     for pair in serverPairs:
         line = "cn" + str(pair[0]) + "\t" + "cn" + str(pair[1]) + "\n"
         lFile.write(line)
     lFile.close()
 
+
 def writeHostFile(nodeList):
-    nFile = open ('nodeFile', 'w')
+    nFile = open('nodeFile', 'w')
     for node in nodeList:
         line = "cn" + str(node) + "\n"
         nFile.write(line)
     nFile.close()
 
+
 def writeServerFile(serverList):
-    sFile = open ('serverFile', 'w')
+    sFile = open('serverFile', 'w')
     for node in serverList:
         line = "cn" + str(node) + "\n"
         sFile.write(line)
     sFile.close()
+
 
 def writeRedisConfigs(serverList):
     pwd = os.getcwd()
     master = "cn" + str(serverList[0])
     for node in serverList:
         nHost = "cn" + str(node)
-        #See if we need to prepare the child
+        # See if we need to prepare the child
         if(nHost != master):
-            #Make child dir if needed
+            # Make child dir if needed
             if not os.path.exists(nHost):
                 os.mkdir(nHost)
-            #Go to child dir
+            # Go to child dir
             os.chdir(nHost)
-            #Write file
+            # Write file
             rFile = open('redis.conf', 'w')
             rFile.write("port 6379\n")
             rFile.write("slaveof " + master + " 6379\n")
             rFile.write("dbfilename dump" + nHost + ".rdb\n")
             rFile.write("slave-read-only no\n")
             rFile.close()
-            #Return to parent dir
+            # Return to parent dir
             os.chdir(pwd)
+
 
 def which(program):
     for path in os.environ['PATH'].split(os.pathsep):
@@ -115,38 +122,40 @@ def which(program):
         if os.path.isfile(binary):
             return binary
 
+
 def writeBashScripts(serverList):
     pwd = os.getcwd()
     master = "cn" + str(serverList[0])
     sFile = open('startRedisServers.sh', 'w')
     eFile = open('endRedisServers.sh', 'w')
-    #Boiler plate
+    # Boiler plate
     bashLine = "#!/bin/bash\n"
     sFile.write(bashLine)
     eFile.write(bashLine)
-    #Do the start script commands
-    #Find redis-server
+    # Do the start script commands
+    # Find redis-server
     rServer = which('redis-server')
     for node in serverList:
         nHost = "cn" + str(node)
         if(nHost != master):
-            #Go to child dir
+            # Go to child dir
             os.chdir(nHost)
-            #Get child dir
+            # Get child dir
             kwd = os.getcwd()
-            #Write command line
+            # Write command line
             cLine = "nohup ssh -n " + nHost
-            cLine += " \'cd " + kwd + "; " + rServer + " " + kwd + "/redis.conf &\' &\n"
+            cLine += " \'cd " + kwd + "; " + rServer + " " + kwd
+            cLine += "/redis.conf &\' &\n"
             sFile.write(cLine)
-            #Return to parent dir
+            # Return to parent dir
             os.chdir(pwd)
         else:
-            #Master is easy. Also it is probably localhost
+            # Master is easy. Also it is probably localhost
             cLine = "nohup ssh -n " + nHost
             cLine += " \'" + rServer + " &' &\n"
             sFile.write(cLine)
     sFile.close()
-    #Now write the end script
+    # Now write the end script
     killswitch = "pkill -f \"redis-server\""
     for node in serverList:
         nHost = "cn" + str(node)
@@ -155,18 +164,20 @@ def writeBashScripts(serverList):
         eFile.write(cLine)
     eFile.close()
 
+
 def main():
+    # Set default servercount
     numServers = 1
     if(len(sys.argv) == 2):
         numServers = int(sys.argv[1])
     nodeList = genNodeList()
     (serverList, serverPairs) = assignServers(nodeList, numServers)
-    #Write LUT file for actual program to use
+    # Write LUT file for actual program to use
     writeLUTFile(serverPairs)
-    #Write nodelist and serverlist if the following scripts aren't used
+    # Write nodelist and serverlist if the following scripts aren't used
     writeHostFile(nodeList)
     writeServerFile(serverList)
-    #Write redis config files and startup/shutdown scripts
+    # Write redis config files and startup/shutdown scripts
     writeRedisConfigs(serverList)
     writeBashScripts(serverList)
 
